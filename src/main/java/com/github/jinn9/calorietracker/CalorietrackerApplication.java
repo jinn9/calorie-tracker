60dc05a527e7cc37911a345046405afa2cc247eb
Jin <jin.dev.1425@gmail.com>
2025-03-18 15:36:15 -0400

    initial commit

---
+++ src/main/java/com/github/jinn9/calorietracker/CalorietrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.jinn9.calorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorietrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorietrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jinn9/calorietracker/CalorietrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jinn9.calorietracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorietrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
